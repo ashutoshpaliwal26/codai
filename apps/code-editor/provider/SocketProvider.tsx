0d3f455bb40f166537af8d97f7ac749feb1dc8d0
ashutoshpaliwal26 <ashutoshpaliwal26@gmail.com>
2025-07-21 21:40:00 +0700

    adding socket cofig

--- apps/code-editor/provider/SocketProvider.tsx
+++ apps/code-editor/provider/SocketProvider.tsx
@@ -2,28 +2,32 @@ import React, { Children, createContext, useContext, useMemo } from 'react'
 import { io, Socket } from 'socket.io-client';
 
 interface SocketContextProps {
-    socket : Socket
+    socket: Socket
 }
 
 const SocketContext = createContext<SocketContextProps | null>(null);
 
 export const useSocket = () => {
     const socket = useContext(SocketContext);
-    if(!socket) return;
+    if (!socket) return;
     return socket.socket;
 }
 
-const SocketProvider = ({children}:{children : React.ReactNode }) => {
+const SocketProvider = ({ children }: { children: React.ReactNode }) => {
     const socket = useMemo<Socket>(() => {
-        return io(process.env.NEXT_PUBLIC_SOCKET_URL)
+        return io(process.env.NEXT_PUBLIC_SOCKET_URL, {
+            path: "/socket.io",
+            transports: ["websocket"],
+            secure: true,
+        })
     }, [])
-  return (
-    <div>
-        <SocketContext.Provider value={{socket}}>
-            {children}
-        </SocketContext.Provider>
-    </div>
-  )
+    return (
+        <div>
+            <SocketContext.Provider value={{ socket }}>
+                {children}
+            </SocketContext.Provider>
+        </div>
+    )
 }
 
 export default SocketProvider
