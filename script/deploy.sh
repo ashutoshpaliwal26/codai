f1c2c64bbec7df8aea13e27f92db28b0845a6108
ashutoshpaliwal26 <ashutoshpaliwal26@gmail.com>
2025-07-24 02:36:28 +0700

    update

--- script/deploy.sh
+++ script/deploy.sh
@@ -13,8 +13,7 @@ sudo docker ps -aq | xargs -r sudo docker rm
 echo " |->>> Removing all Docker images..."
 sudo docker images -q | xargs -r sudo docker rmi -f
 
-echo " |->>> Building and running containers with docker-compose..."
-cd /home/ashutoshpaliwal26/codai  # change this if needed
+echo " |->>> Building and running containers with docker-compose..." # change this if needed
 
 # Use the correct docker compose syntax (space, not dash) for modern Docker versions
 sudo docker compose --env-file .env up --build -d
