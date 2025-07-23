d7c2eac7f02d7f88c6d973eeb40f7c3161bc020a
ashutoshpaliwal26 <ashutoshpaliwal26@gmail.com>
2025-07-23 21:50:53 +0700

    update

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,16 +4,16 @@
 set -e
 
 echo " |->>> Stopping all running Docker containers..."
-docker stop $(docker ps -q) 2>/dev/null || echo " [*] : No running containers."
+sudo docker stop $(docker ps -q) 2>/dev/null || echo " [*] : No running containers."
 
 echo " |->>> Removing all containers..."
-docker rm $(docker ps -aq) 2>/dev/null || echo "No containers to remove."
+sudo docker rm $(docker ps -aq) 2>/dev/null || echo "No containers to remove."
 
 echo " |->>> Removing all Docker images..."
-docker rmi -f $(docker images -q) 2>/dev/null || echo "No images to remove."
+sudo docker rmi -f $(docker images -q) 2>/dev/null || echo "No images to remove."
 
 echo " |->>> Running docker-compose up --build..."
-docker-compose up --rm mongo_db
+sudo docker-compose up --rm mongo_db
 
 echo " |->>> Running Gateway NGINX "
-sudo nginx -t && sudo systemctl restart nginx
+sudo sudo nginx -t && sudo systemctl restart nginx
